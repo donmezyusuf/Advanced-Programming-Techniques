610301c0b537caa16acfef1fb391a2589c071e77
Yusuf Dönmez <yusuf.donmez@outlook.com.tr>
2020-12-13 20:39:41 +0300

    New features and bug fixes

--- Project/app.js
+++ Project/app.js
@@ -5,20 +5,19 @@ class List {
     this.editMode = true;
   }
   EditMode = (id) => {
-    if(this.editMode){
-      console.log("Edit")
+    if (this.editMode && document.getElementById(id)) {
+      console.log("Edit");
       let li_tag = document.getElementById(id);
       let p_tag = li_tag.childNodes[1];
-  
-      if(String(p_tag.outerHTML).charAt(1) !== "p")
-        return;
+
+      if (String(p_tag.outerHTML).charAt(1) !== "p") return;
       let str = p_tag.innerHTML;
-  
+
       //console.log(str);
-  
+
       let container = document.createElement("div");
       container.className = "EditContainer";
-  
+
       let inp = document.createElement("input");
       inp.setAttribute("value", str);
       inp.id = id;
@@ -27,22 +26,21 @@ class List {
       closeEditButton.innerHTML = "Bitti";
       closeEditButton.className = "FinishEditButton";
       closeEditButton.id = id;
-      closeEditButton.setAttribute("onclick","appList.EditCloseMode(id)");
-  
+      closeEditButton.setAttribute("onclick", "appList.EditCloseMode(id)");
+
       container.appendChild(inp);
       container.appendChild(closeEditButton);
-  
+
       //console.log(container);
       li_tag.childNodes[1].outerHTML = container.outerHTML;
-      this.editMode=false;
       //console.log(document.getElementById(id));
     }
-
+    this.editMode = true;
   };
 
   EditCloseMode = (id) => {
     let text = document.getElementById(id).childNodes[1].childNodes[0].value;
-    localStorage.setItem(id, text);
+    localStorage.setItem(id, String(text));
     //console.log(localStorage)
 
     let elt = document.getElementById(id);
@@ -50,29 +48,33 @@ class List {
     console.log(document.getElementById(id));
 
     let add_p = document.createElement("p");
-    add_p.className="ReminderText";
-    add_p.innerHTML=text;
+    add_p.className = "ReminderText";
+    add_p.innerHTML = text;
     document.getElementById(id).appendChild(add_p);
-    
 
+    this.editMode = false;
   };
 
   AddItem = () => {
     const map = new Map(Object.entries(localStorage));
-    while( map.has(this.id)){
+    console.log(map)
+    while (map.has(String(this.id))) {
       this.id = this.id + 1;
+      console.log("arttırdı")
     }
+    console.log(this.id)
 
     let li = document.createElement("li");
     li.className = "ReminderItem";
-    li.setAttribute("id",this.id);
-    li.setAttribute("onclick", "appList.EditMode(id)");
+    li.setAttribute("id", this.id);
+    li.setAttribute("onclick", "appList.EditMode(+"+this.id+")");
 
     let iconButton = document.createElement("button");
     iconButton.className = "iconContainer";
 
     let iconSpan = document.createElement("span");
     iconSpan.className = "icon checkmark-icon";
+    iconSpan.setAttribute("onclick", "appList.DeleteItem(" + this.id + ")");
 
     iconButton.appendChild(iconSpan);
 
@@ -87,6 +89,42 @@ class List {
     document.getElementById("ReminderUL").appendChild(li);
     this.id = this.id + 1;
   };
+
+  DeleteItem = (id) => {
+    console.log(document.getElementById(id));
+    document.getElementById(id).remove();
+    localStorage.removeItem(id);
+  };
+
+  LoadLocalStorage = () => {
+    const map = new Map(Object.entries(localStorage));
+    for (let entry of map) {
+      console.log(entry);
+      let li = document.createElement("li");
+      li.className = "ReminderItem";
+      li.setAttribute("id", entry[0]);
+      li.setAttribute("onclick", "appList.EditMode(id)");
+
+      let iconButton = document.createElement("button");
+      iconButton.className = "iconContainer";
+
+      let iconSpan = document.createElement("span");
+      iconSpan.className = "icon checkmark-icon";
+      iconSpan.setAttribute("onclick", "appList.DeleteItem(" + entry[0] + ")");
+      iconButton.appendChild(iconSpan);
+
+      let text_p = document.createElement("p");
+      text_p.className = "ReminderText";
+
+      text_p.innerHTML = entry[1];
+
+      li.appendChild(iconButton);
+      li.appendChild(text_p);
+  
+      document.getElementById("ReminderUL").appendChild(li);
+    }
+  };
 }
-localStorage.clear();
+
 let appList = new List();
+appList.LoadLocalStorage();
